d5cd6ab2936326136c58b3785368bd56dc0b04d3
Cauchy03 <1481856337@qq.com>
2023-06-07 09:51:56 +0800

    chore: add Proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons' // svg插件
 import { viteMockServe } from 'vite-plugin-mock'
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  // 获取各种环境下的对应变量
+  let env = loadEnv(mode, process.cwd())
   return {
     plugins: [
       vue(),
@@ -15,7 +17,7 @@ export default defineConfig(({ command, mode }) => {
         symbolId: 'icon-[dir]-[name]'
       }),
       viteMockServe({
-        enable: command === 'serve' //保证开发阶段可以使用mock接口
+        localEnabled: command === 'serve' //保证开发阶段可以使用mock接口
       })
     ],
     resolve: {
@@ -29,6 +31,19 @@ export default defineConfig(({ command, mode }) => {
         scss: {
           javascriptEnabled: true,
           additionalData: '@import "./src/styles/variable.scss";'
+        }
+      }
+    },
+    // 代理跨域
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+           //获取数据的服务器地址设置
+           target: env.VITE_SERVE,
+           //需要代理跨域
+           changeOrigin: true,
+           //路径重写
+           rewrite: (path) => path.replace(/^\/api/, ''),
         }
       }
     }
